00b41191a20d1a700190328c37b24f1c85e34ca4
Francis Alcalde <francisarturoalcaldesocole@gmail.com>
2023-01-18 23:51:47 -0300

    modificacion de eventos

--- js/script.js
+++ js/script.js
@@ -1,45 +1,79 @@
 /*Sección Compras en tienda de galletas veganas 
 La tienda tiene 3 variedades de galletas que se venden en cajas de 800gr*/
 
-
-
-
-
-
+// Variables
 let elegida;
 let posicion;
 let pedidoRecup;
 let suma = 0;
 let subtotal;
+let elemento;
+let variedadElegida;
+let conteoIdCarrito = 1;
 
+
+// funcion para cambiar la imagen de la galleta seleccionada
 function cambiarGalleta (galleta, source) {
     galleta.src = source;
 }
+
+// funcion para recuperar el carrito en el local storage
 function recupCarrito () {
     pedidoRecup = JSON.parse(localStorage.getItem("carritoStorage"));
     return pedidoRecup;
 }
 
-function agregarCarrito () {
-    if (pedidoRecup.length > 0){
-        ultimoElemento = pedidoRecup[pedidoRecup.length - 1]
-        const elemento = document.createElement("P");
-        elemento.classList.add("borde");
-        elemento.innerHTML = `Nombre: ${ultimoElemento.nombre} <br> Precio: ${ultimoElemento.precio} <br> Cantidad: ${cantidad.value} `;  
-        subtotal = ultimoElemento.precio * cantidad.value;
-        suma += subtotal;
-        sumaTotal.innerHTML = `Total: ${suma}`;
-        productosPedidos.appendChild(elemento);
-    }
+// funcion para asignar el id de los productos en el carrito y de los botones de eliminar
+function sumaConteoIdCarrito () {
+    conteoIdCarrito ++;
 }
 
-// function sumarTotal() {
-//     for (elemento of pedidoRecup) {
-//         subtotal += elemento.precio * cantidad.value;
-//     }
-// }
+// funcion para sumar el iva a los precios de los productos
+function sumaSubTotal(precio, cantidad) {
+    subtotal = precio * cantidad;
+    suma += subtotal;
+    sumaTotal.innerHTML = `Total: $${suma}`;
+}
+
+// funcion para limpiar el carrito despues de apretar el boton "finalizar compra"
+function limpiarCarrito () {
+    while (carrito.length > 0) {
+    carrito.pop();
+    }
+    suma = 0;
+    productosPedidos.innerHTML = "";
+    sumaSubTotal(0, 0);
+    productosPedidos.appendChild(sumaTotal);
+}
+
+// funcion para que se muestren los productos seleccionados en el carrito
+function agregarCarrito () {
+    limpiarAgregar ()
+
+    carrito.forEach( item => {
+        elemento = document.createElement("DIV");
+        elemento.classList.add("borde");
+        elemento.classList.add("d-flex");
+        elemento.classList.add("align-items-center");
+        elemento.innerHTML += `<img class = "imagenCarrito bordeImagen" src= ${item.imagen}></img>`
+        elemento.innerHTML += `Nombre: ${item.nombre} <br> Precio: $${item.precio} <br> Cantidad: ${item.cantidad} <button id="${item.id}" type="button" class="btn btn-danger me-2 btnEliminar">Eliminar</button>`;  
+        sumaSubTotal(item.precio, item.cantidad); 
+        productosPedidos.appendChild(elemento);
+        sumaConteoIdCarrito();
+    });
+}
+
+// funcion para limpiar el carrito por cada producto que se agrega
+function limpiarAgregar () {
+    suma = 0;
+    sumaSubTotal(0, 0);
+    productosPedidos.innerHTML = "";
+    productosPedidos.appendChild(sumaTotal);
+    conteoIdCarrito = 1;
+}
 
 
+// contructor de array de productos
 
 class variedad {
     constructor(nombre, precio, peso, ingred, imagen, id) {
@@ -48,23 +82,40 @@ class variedad {
         this.peso = peso;
         this.ingred = ingred;
         this.imagen = imagen;
-        this.id = id;
     }
     ivaPrecio() {
         this.precio = this.precio * 1.21;
     }
 }
 
+// array de productos
 const productos = [];
 
-productos.push(new variedad ("Galletas Chips de Chocolate", 2000, 800, "Anacardos, jarabe de arce, chips de chocolate orgánico (azúcar de caña orgánico, licor de cacao orgánico, manteca de cacao orgánico), vainilla, sal marina, bicarbonato de sodio.", "./images/galletaChip Chocolate.jpg", 1));
-productos.push(new variedad ("Galletas Doble Chocolate", 1900, 800, "Anacardos, jarabe de arce, chips de chocolate orgánico (azúcar de caña orgánico, licor de cacao orgánico, manteca de cacao orgánico), cacao orgánico, vainilla, sal marina, bicarbonato de sodio.", "./images/galletaDobleChocolate.jpg", 2));
-productos.push(new variedad ("Galletas Mani", 1800, 800, "Anacardos, jarabe de arce, maní orgánico, vainilla, sal marina, bicarbonato de sodio.", "./images/galletaMani.jpg", 3));
+productos.push(new variedad ("Galletas Chips de Chocolate", 2000, 800, "Anacardos, jarabe de arce, chips de chocolate orgánico (azúcar de caña orgánico, licor de cacao orgánico, manteca de cacao orgánico), vainilla, sal marina, bicarbonato de sodio.", "./images/galletaChipChocolate.jpg"));
+productos.push(new variedad ("Galletas Doble Chocolate", 1900, 800, "Anacardos, jarabe de arce, chips de chocolate orgánico (azúcar de caña orgánico, licor de cacao orgánico, manteca de cacao orgánico), cacao orgánico, vainilla, sal marina, bicarbonato de sodio.", "./images/galletaDobleChocolate.jpg"));
+productos.push(new variedad ("Galletas Mani", 1800, 800, "Anacardos, jarabe de arce, maní orgánico, vainilla, sal marina, bicarbonato de sodio.", "./images/galletaMani.jpg"));
 
+// array y constructor de productos del carrito
+let carrito = [];
+
+class pedido {
+    constructor(nombre, precio, cantidad, imagen, id) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.cantidad = cantidad;
+        this.imagen = imagen;
+        this.id = id;
+    }
+}
+
+
+// activando la funcion de sumar iva a los precios
 for (const variedad of productos) {
     variedad.ivaPrecio();
 }
 
+
+// elementos obtenidos del html para modificar
 let opcionesGalleta = document.getElementsByClassName("variedades");
 let imagen = document.getElementById("imagenGalletas");
 let nombreGalleta = document.getElementById("titulo-galletas");
@@ -76,14 +127,17 @@ let botonAgregar = document.getElementById("agregar-producto");
 let productosPedidos = document.getElementById("productos");
 let carritoBoton = document.getElementById("boton-carrito");
 let sumaTotal = document.getElementById("total");
+let botonComprar = document.getElementById("botonComprar");
+let botonEliminar = document.getElementsByClassName("btnEliminar");
+
 
 cantidad.value = 1;
 
-
+// agregando evento click para que se cambien las imagenes y que se marque la seleccionada
 for (const element of opcionesGalleta) {
     element.addEventListener("click", ()=> {
         
-        let variedadElegida = element.id;   
+        variedadElegida = element.id;   
         elegida = productos.find(variedad => variedad.nombre == variedadElegida);
         posicion = productos.findIndex(variedad => variedad.nombre == variedadElegida);
         cambiarGalleta(imagen, elegida.imagen);
@@ -100,31 +154,56 @@ for (const element of opcionesGalleta) {
     })
 }
 
+// evento boton menos cantidad
 botonMenos.addEventListener("click", ()=> {
     if (cantidad.value > 1){
         cantidad.value --;
     }
 })
 
+// evento boton mas cantidad
 botonMas.addEventListener("click", ()=> {
     cantidad.value ++;
 })
 
-let carrito = [];
 
-class pedido {
-    constructor(nombre, precio, cantidad) {
-        this.nombre = nombre;
-        this.precio = precio;
-        this.cantidad = cantidad;
-    }
-}
-
+// evento del boton agregar para pushear los productos en el array del carrito y que se muestren
 botonAgregar.addEventListener("click", ()=> {
-    carrito.push(new pedido (productos[posicion].nombre, productos[posicion].precio, cantidad.value));
+    if (posicion == undefined) {
+        carrito.push(new pedido (productos[0].nombre, productos[0].precio, cantidad.value, productos[0].imagen, conteoIdCarrito));
+    } else {
+        carrito.push(new pedido (productos[posicion].nombre, productos[posicion].precio, cantidad.value, productos[posicion].imagen, conteoIdCarrito));
+    }
+    for (const element of opcionesGalleta) {
+        
+    }
     localStorage.setItem("carritoStorage", JSON.stringify(carrito));
     recupCarrito();
     agregarCarrito();
+
 })
+
+// evento del boton finalizar compra
+
+botonComprar.addEventListener("click", ()=> {
+    swal({
+        title: "Compra realizada!",
+        text: "Tu pedido ha sido registrado con éxito",
+        icon: "success",
+        button: "Aceptar",
+    });
+    limpiarCarrito ();
+    conteoIdCarrito = 1;
+    localStorage.clear();
+})
+
+
+// evento de los botones eliminar 
+productosPedidos.addEventListener("click", (event) => {
+    botonId = event.target.id;
+        carrito = carrito.filter((item) => item.id!= botonId);
+        agregarCarrito();
+})
+
 
 
